f017d2c44839b0739aa7617cef017ebb10463bc0
tiinach <96144093+tiinach@users.noreply.github.com>
2022-02-02 21:34:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import FetchAPI from './components/API/FetchApi';
+
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <FetchAPI/>
+   
+       </div>
+  );
+};
+
+export default App;
