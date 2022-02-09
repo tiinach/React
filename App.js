1d250c4f088b5b365ff5fc4ab259dd94f7cd23fc
tiinach <96144093+tiinach@users.noreply.github.com>
2022-02-09 09:16:53 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import FetchAPI from"./components/button";
+import Card from"./components/card";
+
+
+export default function App() {
+  return (
+    <div className="App">
+    <Card/>
+
+    </div>
+  );
+}
